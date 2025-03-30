85e0c82d22b0feea475df8ad52a42477858863dc
Mel Shu <mel.y.shu@gmail.com>
2025-03-30 12:31:45 -0400

    works but ain't too fast

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@
 #include <map>
 #include <optional>
 #include <fstream>
+#include <queue>
 
 using int_t = int8_t;
 constexpr int_t R = 11;
@@ -65,6 +66,21 @@ struct Position
 
         return ret;
     }
+
+    friend std::ostream &operator<<(std::ostream &os, const Position &pos)
+    {
+        return os << "Position(r=" << std::to_string(pos.r)
+                  << ", c=" << std::to_string(pos.c) << ")";
+    }
+
+    friend std::istream &operator>>(std::istream &is, Position &pos)
+    {
+        int r, c;
+        is >> r >> c;
+        pos.r = r;
+        pos.c = c;
+        return is;
+    }
 };
 
 void throw_error(char s, int_t r, int_t c, int line_no)
@@ -80,92 +96,7 @@ struct Board
     std::bitset<N> cells;
     Position cat_position;
 
-    static Board load(std::istream &is)
-    {
-        Board ret;
-        char s;
-        bool cat_found = false;
-
-        for (int_t r = 0; r < R; ++r)
-        {
-            for (int_t c = 0; c < C; ++c)
-            {
-                Position p(r, c);
-
-                is >> s;
-                if (s == 'C')
-                {
-                    if (cat_found)
-                    {
-                        throw_error(s, r, c, __LINE__);
-                    }
-
-                    ret.cat_position = Position(r, c);
-                    cat_found = true;
-                    ret.cells[p.idx()] = false;
-                }
-                else if (s == '#')
-                {
-                    ret.cells[p.idx()] = true;
-                }
-                else if (s == '-')
-                {
-                    ret.cells[p.idx()] = false;
-                }
-                else
-                {
-                    throw_error(s, r, c, __LINE__);
-                }
-            }
-        }
-
-        if (!cat_found)
-        {
-            throw_error('C', -1, -1, __LINE__);
-        }
-
-        return ret;
-    }
-
-    void dump(std::ostream &os) const
-    {
-        for (int_t r = 0; r < R; ++r)
-        {
-            if (r % 2)
-            {
-                os << ' ';
-            }
-
-            for (int_t c = 0; c < C; ++c)
-            {
-                Position p(r, c);
-
-                if (c)
-                {
-                    os << ' ';
-                }
-
-                if (cat_position == p)
-                {
-                    os << 'C';
-                }
-                else if (cells[p.idx()])
-                {
-                    os << '#';
-                }
-                else
-                {
-                    os << '-';
-                }
-            }
-
-            os << std::endl;
-        }
-
-        os << std::endl;
-    }
-
-    bool move_cat()
+    std::optional<Position> move_cat()
     {
         struct SearchState
         {
@@ -230,7 +161,7 @@ struct Board
         {
             if (moves2search_state.empty())
             {
-                return false;
+                return std::nullopt;
             }
 
             std::optional<Position> best_move;
@@ -248,7 +179,7 @@ struct Board
             if (best_move.has_value())
             {
                 cat_position = *best_move;
-                return true;
+                return best_move;
             }
 
             auto it = moves2search_state.begin();
@@ -278,6 +209,90 @@ struct Board
 
         return false;
     }
+
+    friend std::istream &operator>>(std::istream &is, Board &board)
+    {
+        char s;
+        bool cat_found = false;
+
+        for (int_t r = 0; r < R; ++r)
+        {
+            for (int_t c = 0; c < C; ++c)
+            {
+                Position p(r, c);
+
+                is >> s;
+                if (s == 'C')
+                {
+                    if (cat_found)
+                    {
+                        throw_error(s, r, c, __LINE__);
+                    }
+
+                    board.cat_position = Position(r, c);
+                    cat_found = true;
+                    board.cells[p.idx()] = false;
+                }
+                else if (s == '#')
+                {
+                    board.cells[p.idx()] = true;
+                }
+                else if (s == '-')
+                {
+                    board.cells[p.idx()] = false;
+                }
+                else
+                {
+                    throw_error(s, r, c, __LINE__);
+                }
+            }
+        }
+
+        if (!cat_found)
+        {
+            throw_error('C', -1, -1, __LINE__);
+        }
+
+        return is;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Board &board)
+    {
+        for (int_t r = 0; r < R; ++r)
+        {
+            if (r % 2)
+            {
+                os << ' ';
+            }
+
+            for (int_t c = 0; c < C; ++c)
+            {
+                Position p(r, c);
+
+                if (c)
+                {
+                    os << ' ';
+                }
+
+                if (board.cat_position == p)
+                {
+                    os << 'C';
+                }
+                else if (board.cells[p.idx()])
+                {
+                    os << '#';
+                }
+                else
+                {
+                    os << '-';
+                }
+            }
+
+            os << std::endl;
+        }
+
+        return os;
+    }
 };
 
 struct BasePlayer
@@ -292,18 +307,166 @@ struct InteractivePlayer : BasePlayer
     {
         std::cout << "Please enter your move (r c):" << std::endl;
 
-        int r, c;
-        std::cin >> r >> c;
-        return Position(r, c);
+        Position pos;
+        std::cin >> pos;
+        return pos;
     }
 };
 
 struct AutoPlayer : BasePlayer
 {
+    std::deque<Position> cached_moves;
+
+    static int_t get_min_cat_moves(const Board &board)
+    {
+        std::set<Position> visited;
+        std::set<Position> current;
+
+        visited.insert(board.cat_position);
+        current.insert(board.cat_position);
+
+        int_t num_cat_moves = 0;
+        while (true)
+        {
+            if (current.empty())
+            {
+                return N;
+            }
+
+            for (const auto p : current)
+            {
+                if (p.is_edge())
+                {
+                    return num_cat_moves;
+                }
+            }
+
+            std::set<Position> new_current;
+            for (const auto p : current)
+            {
+                for (const auto pp : p.neighbors())
+                {
+                    if (!visited.count(pp) && !board.cells[pp.idx()])
+                    {
+                        new_current.insert(pp);
+                    }
+                }
+            }
+
+            current = new_current;
+            visited.insert(current.begin(), current.end());
+            ++num_cat_moves;
+        }
+    }
+
+    struct SearchState
+    {
+        Board board;
+        mutable int_t cached_min_cat_moves = -1;
+        int_t num_moves = 0;
+        std::shared_ptr<SearchState> previous;
+        std::optional<Position> move;
+
+        int_t min_cat_moves() const
+        {
+            if (cached_min_cat_moves == -1)
+            {
+                cached_min_cat_moves = get_min_cat_moves(board);
+            }
+
+            return cached_min_cat_moves;
+        }
+
+        int priority() const
+        {
+            return min_cat_moves();
+        }
+
+        bool operator<(const SearchState &other) const
+        {
+            return priority() < other.priority();
+        }
+
+        friend std::ostream &operator<<(std::ostream &os, const SearchState &search_state)
+        {
+            os << "State(" << std::endl;
+            os << "  priority=" << search_state.priority() << "," << std::endl;
+            os << "  num_moves=" << std::to_string(search_state.num_moves) << "," << std::endl;
+            os << "  previous=" << (search_state.previous ? "yes" : "no") << "," << std::endl;
+            ;
+            os << "  move=";
+            if (search_state.move)
+            {
+                os << *search_state.move;
+            }
+            else
+            {
+                os << "none";
+            }
+            os << "," << std::endl;
+            os << "  min_cat_moves=" << std::to_string(search_state.min_cat_moves()) << "," << std::endl;
+            os << ") with board:" << std::endl;
+            os << search_state.board;
+            return os;
+        }
+    };
+
     Position get_move(const Board &board) override
     {
-        // To... uhh... make smarter.
-        return Position(0, 0);
+        if (!cached_moves.empty())
+        {
+            auto ret = cached_moves.back();
+            cached_moves.pop_back();
+            return ret;
+        }
+
+        std::priority_queue<SearchState> pq;
+        SearchState first_state;
+        first_state.board = board;
+        pq.emplace(first_state);
+
+        while (true)
+        {
+            std::clog << "Popping top of " << pq.size() << " states: " << std::endl;
+            auto current_ptr = std::make_shared<SearchState>(pq.top());
+            pq.pop();
+            auto &current_state = *current_ptr;
+            std::clog << current_state << std::endl;
+
+            if (current_state.cached_min_cat_moves == N)
+            {
+                std::clog << "Solution found!" << std::endl;
+                for (auto ptr = current_ptr; ptr->move && ptr->previous; ptr = ptr->previous)
+                {
+                    cached_moves.push_back(*ptr->move);
+                }
+
+                auto ret = cached_moves.back();
+                cached_moves.pop_back();
+                return ret;
+            }
+
+            for (int_t r = 0; r < R; ++r)
+            {
+                for (int_t c = 0; c < C; ++c)
+                {
+                    Position p(r, c);
+                    if (!current_state.board.cells[p.idx()] && !(p == current_state.board.cat_position))
+                    {
+                        SearchState next_state;
+                        Board next_board = current_state.board;
+                        next_board.cells[p.idx()] = true;
+                        next_board.move_cat();
+                        next_state.board = next_board;
+                        next_state.num_moves = current_state.num_moves + 1;
+                        next_state.previous = current_ptr;
+                        next_state.move = p;
+
+                        pq.push(next_state);
+                    }
+                }
+            }
+        }
     }
 };
 
@@ -315,15 +478,14 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    auto board = [file_path = argv[1]]()
-    {
-        std::ifstream ifs(file_path);
-        return Board::load(ifs);
-    }();
+    Board board;
+    std::ifstream ifs(argv[1]);
+    ifs >> board;
 
-    board.dump(std::cout);
+    std::cout << board << std::endl;
 
-    std::unique_ptr<BasePlayer> player = std::make_unique<InteractivePlayer>();
+    // std::unique_ptr<BasePlayer> player = std::make_unique<InteractivePlayer>();
+    std::unique_ptr<BasePlayer> player = std::make_unique<AutoPlayer>();
 
     while (true)
     {
@@ -335,18 +497,20 @@ int main(int argc, char *argv[])
 
         while (true)
         {
-            if (board.move_player(player->get_move(board)))
+            const auto move = player->get_move(board);
+            if (board.move_player(move))
             {
+                std::cout << "Accepted player's move: " << move << std::endl;
                 break;
             }
         }
 
-        board.dump(std::cout);
+        std::cout << board << std::endl;
 
-        if (board.move_cat())
+        if (const auto move = board.move_cat())
         {
-            std::cout << "Cat moved:" << std::endl;
-            board.dump(std::cout);
+            std::cout << "Cat moved: " << *move << std::endl;
+            std::cout << board << std::endl;
         }
         else
         {
